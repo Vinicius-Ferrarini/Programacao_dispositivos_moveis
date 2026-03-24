c5dceff44d57d0407f8499d32bbad6da186dde40
Vinicius-Ferrarini <viniciusferrarini48@gmail.com>
2026-03-24 17:53:53 -0300

    movido dados para arquivo

---
+++ 002_lista_exercicios/app/src/data/dados.js
@@ -0,0 +1,17 @@
+export const dadosCategorias = [
+  { id: '1', nome: 'Eletrônicos', imagem: require('../../../assets/images/categoria_eletronicos.jpg') },
+  { id: '2', nome: 'Roupas', imagem: require('../../../assets/images/categoria_roupas.png') },
+  { id: '3', nome: 'Casa', imagem: require('../../../assets/images/categoria_casa.jpg') },
+  { id: '4', nome: 'Carro', imagem: require('../../../assets/images/categoria_carro.jpg') },
+  { id: '5', nome: 'Livros', imagem: require('../../../assets/images/categoria_livros.jpg') },
+  { id: '6', nome: 'Esportes', imagem: require('../../../assets/images/categoria_esportes.jpg') },
+];
+
+export const dadosProdutos = [
+  { id: '1', nome: 'Tênis Esportivo', preco: 'R$ 199,90', imagem: require('../../../assets/images/categoria_esportes.jpg') },
+  { id: '2', nome: 'Relógio Digital', preco: 'R$ 250,00', imagem: require('../../../assets/images/categoria_esportes.jpg') },
+  { id: '3', nome: 'Mochila Preta', preco: 'R$ 120,00', imagem: require('../../../assets/images/categoria_esportes.jpg') },
+  { id: '4', nome: 'Fone Bluetooth', preco: 'R$ 89,90', imagem: require('../../../assets/images/categoria_esportes.jpg') },
+  { id: '5', nome: 'Câmera', preco: 'R$ 1500,00', imagem: require('../../../assets/images/categoria_esportes.jpg') },
+  { id: '6', nome: 'Óculos', preco: 'R$ 59,90', imagem: require('../../../assets/images/categoria_esportes.jpg') },
+];
